3144557c514f3bf6f035d1cf73ea017e861b28c8
Massi <massinisabennai@gmail.com>
2022-02-21 15:42:53 +0100

    added packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+"use strict";
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  images: {
+    domains: ["www.notion.so", "images.unsplash.com"],
+  },
+  async redirects() {
+    return [
+      // redirect the index page to our notion test suite
+      {
+        source: "/",
+        destination: "/0265184804eb43388231161927864e13",
+        // don't set permanent to true because it will get cached by the browser
+        // while developing on localhost
+        permanent: false,
+      },
+    ];
+  },
+});
